ea18f2f3083773ad3f1a1c08a8f02affcc0eceec
maryamchegini1987 <m_chegini1987@yahoo.com>
2023-09-24 22:55:42 +0330

    create custom hooks for react query and dublicate cabins btn

--- src/features/cabins/CabinTable-v1.jsx
+++ src/features/cabins/CabinTable-v1.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+import { useQuery } from "@tanstack/react-query";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
-import { useCabins } from "./useCabins";
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
 
@@ -27,7 +28,14 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-  const { isLoading, cabins } = useCabins();
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabins"],
+    queryFn: getCabins,
+  });
 
   if (isLoading) return <Spinner />;
   return (
@@ -40,8 +48,9 @@ function CabinTable() {
         <div>Discount</div>
         <div></div>
       </TableHeader>
-      {cabins &&
-        cabins.map((cabin) => <CabinRow cabin={cabin} key={cabin.id} />)}
+      {cabins && cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
     </Table>
   );
 }
